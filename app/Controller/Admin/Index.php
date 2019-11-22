8726fa3cabf22d397b5bab2a727ee14336255e49
hooshys <2550164142@qq.com>
2019-11-22 16:19:52 +0800

    项目搭建

---
+++ app/Controller/Admin/Index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 2019/11/22
+ * Time: 16:06
+ */
+namespace app\controller\admin;
+
+class Index{
+    public function index(){
+        echo "hello index";
+    }
+}
